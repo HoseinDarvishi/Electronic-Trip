d703a28f9c2286add6253298cc6093dc6bcde48c
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-20 09:39:34 -0800

    Implementation Driver Mapping

---
+++ ET.Data/Mappings/DriverMapping.cs
@@ -0,0 +1,19 @@
+﻿using ET.Domain.DriverAgg;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ET.Data.Mappings
+{
+   public class DriverMapping : EntityTypeConfiguration<Driver>
+   {
+      public DriverMapping()
+      {
+         this.ToTable("Drivers");
+         this.HasKey(d => d.DriverId);
+         this.Property(d => d.FullName).HasMaxLength(300).IsRequired();
+         this.Property(d => d.Age).IsRequired();
+         this.Property(d => d.ExprienceYears).IsRequired();
+
+         this.HasMany(d => d.Cars).WithRequired(c => c.Driver).HasForeignKey(c => c.DriverId);
+      }
+   }
+}
