3e939f62531041bce1cd155ab909b3df9a8a67dc
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-19 23:24:23 -0800

    adding User Model

---
+++ ET.Domain/UserAgg/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ET.Domain.UserAgg
+{
+   public class User
+   {
+      #region Ctor
+      private User() { }
+      public User(string fullName , string email)
+      {
+         FullName = fullName;
+         Email = email;
+         RegisterDate = DateTime.Now;
+      }
+      #endregion
+
+      public int UserId { get; private set; }
+      public string FullName { get; private set; }
+      public string Email { get; private set; }
+      public bool IsActive { get; private set; }
+      public DateTime RegisterDate { get; private set; }
+
+      #region Relations
+      #endregion
+
+      #region Edit
+      public void Active() => IsActive = true;
+      public void DeActive() => IsActive = false;
+      #endregion
+   }
+}
