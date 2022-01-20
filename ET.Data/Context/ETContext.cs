67e47ffbebc161565ec29696a67fc9a56a7db599
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-19 23:34:01 -0800

    implementation Context in Data layer

---
+++ ET.Data/Context/ETContext.cs
@@ -0,0 +1,17 @@
+﻿using ET.Domain.UserAgg;
+using System.Data.Entity;
+
+namespace ET.Data.Context
+{
+   public class ETContext : DbContext
+   {
+      public ETContext() : base() {}
+
+      public virtual DbSet<User> Users { get; private set; }
+
+      protected override void OnModelCreating(DbModelBuilder modelBuilder)
+      {
+         base.OnModelCreating(modelBuilder);
+      }
+   }
+}
