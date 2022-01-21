9d0fdb280831da973cf573c58fc47310f7666316
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-21 01:58:05 -0800

    implementation Role Mapping

---
+++ ET.Data/Mappings/RoleMapping.cs
@@ -0,0 +1,17 @@
+﻿using ET.Domain.RoleAgg;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ET.Data.Mappings
+{
+   public class RoleMapping : EntityTypeConfiguration<Role>
+   {
+      public RoleMapping()
+      {
+         this.ToTable("Roles");
+         this.HasKey<int>(x => x.RoleId);
+         this.Property(x => x.RoleTitle).HasMaxLength(300).IsRequired();
+
+         this.HasMany(x => x.Users).WithRequired(x => x.Role).HasForeignKey(x => x.RoleId);
+      }
+   }
+}
--- ET.Domain/UserAgg/User.cs
+++ ET.Domain/UserAgg/User.cs
@@ -1,4 +1,5 @@
 ﻿using ET.Domain.CarAgg;
+using ET.Domain.RoleAgg;
 using System;
 using System.Collections.Generic;
 
@@ -8,12 +9,13 @@ namespace ET.Domain.UserAgg
    {
       #region Ctor
       private User() { }
-      public User(string fullName ,string userName , string email,string password)
+      public User(string fullName ,string userName , string email,string password,int roleId)
       {
          FullName = fullName;
          UserName = userName;
          Email = email;
          Password = password;
+         RoleId = roleId;
          RegisterDate = DateTime.Now;
       }
       #endregion
@@ -23,16 +25,26 @@ namespace ET.Domain.UserAgg
       public string UserName { get; private set; }
       public string Password { get; private set; }
       public string Email { get; private set; }
+      public int RoleId { get;private set; }
       public bool IsActive { get; private set; }
       public DateTime RegisterDate { get; private set; }
 
       #region Relations
       public virtual List<Car> Cars { get; set; }
+      public virtual Role Role { get; set; }
       #endregion
 
       #region Edit
       public void Active() => IsActive = true;
+
       public void DeActive() => IsActive = false;
+
+      public void Edit(string fullName, string userName, string email)
+      {
+         FullName = fullName;
+         UserName = userName;
+         Email = email;
+      }
       #endregion
    }
 }
