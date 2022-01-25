be6e92fb1b3ca73818838a6db1cdfaf64fa41ecc
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-25 02:05:34 -0800

    Adding Web Layer (Asp.net Mvc)

---
+++ ET.Web/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using ET.IoC;
+using System.Configuration;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ET.Web
+{
+   public class MvcApplication : System.Web.HttpApplication
+   {
+      protected void Application_Start()
+      {
+         // Dependency Injection By Autofac
+         var autofac = new ETBootstrapper();
+         autofac.Container().RegisterControllers(typeof(MvcApplication).Assembly);
+         autofac.Container().RegisterFilterProvider();
+         autofac.Container().RegisterSource(new ViewRegistrationSource());
+         autofac.Configure(ConfigurationManager.ConnectionStrings["ElectronicTrip"].ConnectionString);
+         autofac.Resolve();
+
+         AreaRegistration.RegisterAllAreas();
+         FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+         BundleConfig.RegisterBundles(BundleTable.Bundles);
+      }
+   }
+}
