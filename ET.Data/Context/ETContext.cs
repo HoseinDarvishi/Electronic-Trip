e997d5a6527fa23c7fa8d3de53ebc22ca1b53585
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-25 02:06:28 -0800

    Adding IoC Layer And Configurating it by Autofac

--- ET.Data/Context/ETContext.cs
+++ ET.Data/Context/ETContext.cs
@@ -9,7 +9,7 @@ namespace ET.Data.Context
 {
    public class ETContext : DbContext
    {
-      public ETContext() : base("Data Source=.\\SQL2019;Initial Catalog=Electronic-Trip;Integrated Security=True;") { }
+      public ETContext(string connectionString) : base(connectionString) { }
 
       public virtual DbSet<User> Users { get; set; }
       public virtual DbSet<Car> Cars { get; set; }
---
+++ ET.IoC/ETBootstrapper.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using ET.Constracts.CarConstracts;
+using ET.Constracts.PermissionContracts;
+using ET.Constracts.RequestContracts;
+using ET.Constracts.RoleConstracts;
+using ET.Constracts.UserContracts;
+using ET.Data.Context;
+using ET.Data.Repositories;
+using ET.Domain.CarAgg;
+using ET.Domain.RequestAgg;
+using ET.Domain.RoleAgg;
+using ET.Domain.UserAgg;
+using ET.Service.Services;
+using System.Web.Mvc;
+
+namespace ET.IoC
+{
+   public class ETBootstrapper
+   {
+      private ContainerBuilder builder = new ContainerBuilder();
+
+      public ContainerBuilder Container() => builder;
+
+      public void Resolve() 
+      {
+         var container = builder.Build();
+         DependencyResolver.SetResolver(new AutofacDependencyResolver(container)); 
+      }
+
+      public void Configure(string connectionString)
+      {
+         // Repositories : 
+         builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerRequest();
+         builder.RegisterType<CarRepository>().As<ICarRepository>().InstancePerRequest();
+         builder.RegisterType<RoleRepository>().As<IRoleRepository>().InstancePerRequest();
+         builder.RegisterType<RequestRepository>().As<IRequestRepository>().InstancePerRequest();
+
+         // Services :
+         builder.RegisterType<UserService>().As<IUserService>().InstancePerRequest();
+         builder.RegisterType<CarService>().As<ICarService>().InstancePerRequest();
+         builder.RegisterType<RoleService>().As<IRoleService>().InstancePerRequest();
+         builder.RegisterType<RequestService>().As<IRequestService>().InstancePerRequest();
+         builder.RegisterType<PermissionExposer>().As<IPermissionExposer>().InstancePerLifetimeScope();
+
+         builder.Register(c => new ETContext(connectionString)).As<ETContext>().InstancePerRequest();
+      }
+   }
+}
