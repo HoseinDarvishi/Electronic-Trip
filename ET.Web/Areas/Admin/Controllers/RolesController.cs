7dffb63cccbe4fdd3d12b4d58f22337f4587d96b
Hossein Darvishi <HosseinDarvishi138@gmail.com>
2022-01-29 03:25:18 -0800

    Implementation Role List View

---
+++ ET.Web/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,23 @@
+﻿using ET.Constracts.RoleConstracts;
+using System.Web.Mvc;
+
+namespace ET.Web.Areas.Admin.Controllers
+{
+   public class RolesController : Controller
+   {
+      private readonly IRoleService _roleService;
+
+      #region Ctor
+      public RolesController(IRoleService roleService)
+      {
+         _roleService = roleService;
+      } 
+      #endregion
+
+      public ActionResult Index(string roleTitle)
+      {
+         var roles = _roleService.GetAll(roleTitle);
+         return View(roles);
+      }
+   }
+}
